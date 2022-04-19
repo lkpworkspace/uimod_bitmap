ee296ebfac1d2b16967d762a8777e16600f79304
lkp <781171312@qq.com>
2022-04-19 22:21:48 +0800

    update

--- com/uimod_bitmap.cpp
+++ com/uimod_bitmap.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <memory>
+#include <chrono>
 #include <functional>
 #include <unordered_map>
 
@@ -21,14 +22,14 @@ pb_msg_ptr_t create_msg(const std::string& msg_name) {
 }
 
 pb_msg_ptr_t draw(std::vector<pb_msg_ptr_t> in_args) {
-    auto res = std::make_shared<bp::Int>();
+    auto res = std::make_shared<bp::Int32>();
     res->set_var(0);
     if (in_args.size() != 3) {
         res->set_var(-1);
         return res;
     }
-    auto width = std::dynamic_pointer_cast<::bp::Int>(in_args[0]);
-    auto height = std::dynamic_pointer_cast<::bp::Int>(in_args[1]);
+    auto width = std::dynamic_pointer_cast<::bp::Int32>(in_args[0]);
+    auto height = std::dynamic_pointer_cast<::bp::Int32>(in_args[1]);
     auto pixels = std::dynamic_pointer_cast<::bp::UInt32Ary>(in_args[2]);
     Json::Value v;
     v["command"] = "draw";
@@ -42,23 +43,30 @@ pb_msg_ptr_t draw(std::vector<pb_msg_ptr_t> in_args) {
 }
 
 pb_msg_ptr_t example(void) {
-    auto res = std::make_shared<bp::Int>();
+    auto res = std::make_shared<bp::Int32>();
     res->set_var(0);
-    static int width = 10;
-    static int height = 10;
-    static std::vector<uint32_t> pixels = {
-        0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0,
-        0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff,
-        0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0,
-        0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff,
-        0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0,
-        0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff,
-        0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0,
-        0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff,
-        0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0,
-        0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff, 0x0, 0xffffffff,
-    };
+    static bool is_init = false;
+    static int width = 144;
+    static int height = 100;
+    static std::vector<uint32_t> pixels;
     static auto last_time = std::chrono::steady_clock::now();
+    if (!is_init) {
+        is_init = true;
+        pixels.clear();
+        uint32_t color = 0x0;
+        for (int i = 0; i < width; ++i) {
+            if (i % 2 == 0) {
+                color = 0x0;
+            } else {
+                color = 0xffffffff;
+            }
+            for (int j = 0; j < height; ++j) {
+                pixels.emplace_back(color);
+                color = ~color;
+            }
+        }
+    }
+
     int swap_time_ms = 1000;
     auto now = std::chrono::steady_clock::now();
     auto cost_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time).count();
